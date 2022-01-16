b1fb71f84539e3023c8149a705112e6c23c82304
Paul Brady <pablo.brady@gmail.com>
2022-01-15 23:44:10 -0800

    First step

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 var express = require('express');
-const fs = require('fs');
-const http = require('http');
-
-
-const fileOptions = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now())
-  }
-};
-
-
 var app = express();
-
 
 app.use(express.static(__dirname + '/dist'));
 
-
-var httpServer = http.createServer(app);
-
-httpServer.listen(80);
+app.listen(3000);
